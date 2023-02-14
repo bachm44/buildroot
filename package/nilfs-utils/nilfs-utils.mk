92a98cc2e5ea9a72454bc1bed361d9e3836ed748
Bartłomiej Chmiel <bachm44@gmail.com>
2023-02-14 20:49:50 +0100

    feat: include custom nilfs-utils package in build

--- package/nilfs-utils/nilfs-utils.mk
+++ package/nilfs-utils/nilfs-utils.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-NILFS_UTILS_VERSION = 2.2.9
+NILFS_UTILS_VERSION = 2.3.0-dev-5afeb9c
 NILFS_UTILS_SOURCE = nilfs-utils-$(NILFS_UTILS_VERSION).tar.bz2
-NILFS_UTILS_SITE = https://nilfs.sourceforge.io/download
+NILFS_UTILS_SITE = https://github.com/bachm44/nilfs-utils/releases/download/$(NILFS_UTILS_VERSION)
 NILFS_UTILS_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
 NILFS_UTILS_LICENSE_FILES = COPYING
 
