c10fe2158c59dd0ec646bb03561766a5f4067555
Bartłomiej Chmiel <bachm44@gmail.com>
2023-02-24 19:07:51 +0100

    bump nilfs-utils version to 2.3.0-dev-32e603c

--- package/nilfs-utils/nilfs-utils.mk
+++ package/nilfs-utils/nilfs-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NILFS_UTILS_VERSION = 2.3.0-dev-5afeb9c
+NILFS_UTILS_VERSION = 2.3.0-dev-32e603c
 NILFS_UTILS_SOURCE = nilfs-utils-$(NILFS_UTILS_VERSION).tar.bz2
 NILFS_UTILS_SITE = https://github.com/bachm44/nilfs-utils/releases/download/$(NILFS_UTILS_VERSION)
 NILFS_UTILS_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
