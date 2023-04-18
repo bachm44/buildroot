9ede9d0c15b820c305ccd09ebbb23ba04034abfa
Bartłomiej Chmiel <bachm44@gmail.com>
2023-04-18 17:40:14 +0200

    refactor mount nilfs script

--- board/incvis/incvis/rootfs-overlay/mount_nilfs.sh
+++ board/incvis/incvis/rootfs-overlay/mount_nilfs.sh
@@ -5,7 +5,7 @@ set -x # logging
 IFS=$'\n\t'
 
 FS_MNT_DIR=/mnt/nilfs2
-FS_FILE_SIZE=1052672
+FS_FILE_SIZE=500M
 FS_BIN_FILE=/nilfs2.bin
 LOOP_INTERFACE=/dev/loop0
 
@@ -15,8 +15,3 @@ losetup -P $LOOP_INTERFACE $FS_BIN_FILE
 mkfs.nilfs2 $LOOP_INTERFACE -B 16
 mkdir -p $FS_MNT_DIR
 mount -t nilfs2 $LOOP_INTERFACE $FS_MNT_DIR
-
-echo 'incvis' > /mnt/nilfs2/f1
-echo 'incvis' > /mnt/nilfs2/f2
-echo 'incvis' > /mnt/nilfs2/f3
-echo '1' > /mnt/nilfs2/f3
