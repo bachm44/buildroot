04e28e61c4f22af51375e9fb003cc79fd8656b46
Bartłomiej Chmiel <bachm44@gmail.com>
2023-05-02 14:17:33 +0200

    bump nilfs-utils to send vblocknr and cno to NILFS_IOCTL_DEDUP

--- package/nilfs-utils/nilfs-utils.mk
+++ package/nilfs-utils/nilfs-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NILFS_UTILS_VERSION = 2.3.0-dev-d2fdc6d1566430c623a14a270cd1829ceff513de
+NILFS_UTILS_VERSION = 2.3.0-dev-d847bb7a56afb6827e8a59b80560e7497f0c72fe
 NILFS_UTILS_SOURCE = nilfs-utils-$(NILFS_UTILS_VERSION).tar.bz2
 NILFS_UTILS_SITE = https://github.com/bachm44/nilfs-utils/releases/download/$(NILFS_UTILS_VERSION)
 NILFS_UTILS_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
