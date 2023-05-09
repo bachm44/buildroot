dffd825b03c7e7d3241310d8aa1a9967204398cb
Bartłomiej Chmiel <bachm44@gmail.com>
2023-05-09 23:19:59 +0200

    bump gen_file

--- package/gen_file/gen_file.mk
+++ package/gen_file/gen_file.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GEN_FILE_VERSION = 1.0.5-dev-f917d9543438713e42da879d70290e62c50d756c
+GEN_FILE_VERSION = 1.0.5-dev-aa8ab56514bd6b4bf7ac5669b620c53b604fcf82
 GEN_FILE_SOURCE = gen_file-$(GEN_FILE_VERSION).tar.gz
 GEN_FILE_SITE = https://github.com/bachm44/gen_file/releases/download/$(GEN_FILE_VERSION)
 GEN_FILE_LICENSE = GPL-3.0+
