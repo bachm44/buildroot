4d1460002cf539430345bfe23cb0544338376000
Bartłomiej Chmiel <bachm44@gmail.com>
2023-05-16 19:16:14 +0200

    bump nilfs-utils to fix one char block bug

--- package/nilfs-utils/nilfs-utils.mk
+++ package/nilfs-utils/nilfs-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NILFS_UTILS_VERSION = 2.3.0-dev-876451c43d425cf5ad8c14b085ef311f1aa0caa0
+NILFS_UTILS_VERSION = 2.3.0-dev-50371db9312c82524fbe3f305e78316b28ea1cd7
 NILFS_UTILS_SOURCE = nilfs-utils-$(NILFS_UTILS_VERSION).tar.bz2
 NILFS_UTILS_SITE = https://github.com/bachm44/nilfs-utils/releases/download/$(NILFS_UTILS_VERSION)
 NILFS_UTILS_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
