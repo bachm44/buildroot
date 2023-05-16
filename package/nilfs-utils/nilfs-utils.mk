1a91294817db57a1a87750e6a550b08eadb24ceb
Bartłomiej Chmiel <bachm44@gmail.com>
2023-05-16 14:26:44 +0200

    bump nilfs-utils to support empty block skip

--- package/nilfs-utils/nilfs-utils.mk
+++ package/nilfs-utils/nilfs-utils.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NILFS_UTILS_VERSION = 2.3.0-dev-d847bb7a56afb6827e8a59b80560e7497f0c72fe
+NILFS_UTILS_VERSION = 2.3.0-dev-876451c43d425cf5ad8c14b085ef311f1aa0caa0
 NILFS_UTILS_SOURCE = nilfs-utils-$(NILFS_UTILS_VERSION).tar.bz2
 NILFS_UTILS_SITE = https://github.com/bachm44/nilfs-utils/releases/download/$(NILFS_UTILS_VERSION)
 NILFS_UTILS_LICENSE = GPL-2.0+ (programs), LGPL-2.1+ (libraries)
